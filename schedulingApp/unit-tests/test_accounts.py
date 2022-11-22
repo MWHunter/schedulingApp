c0a5d5ab41ca31d084c01f798c05ff1068c8d083
Svedek <naroon10@gmail.com>
2022-11-22 16:03:48 -0600

    Added unit-test folder and started test_accounts script

---
+++ schedulingApp/unit-tests/test_accounts.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+
+class CreateAccount:
+    def test_emptyUsernameAndPassword(self):
+        pass
+
+    def test_emptyUsername(self):
+        pass
+
+    def test_emptyPassword(self):
+        pass
+
+    def test_validInfo(self):
+        pass
+
+
+class EditAccount:
+    def test_emptyUsernameAndPassword(self):
+        pass
+
+    def test_emptyUsername(self):
+        pass
+
+    def test_emptyPassword(self):
+        pass
+
+    def test_validInfo(self):
+        pass
+
+class DeleteAccount:
+    def test_emptyUsernameAndPassword(self):
+        pass
+
+    def test_emptyUsername(self):
+        pass
+
+    def test_emptyPassword(self):
+        pass
+
+    def test_validInfo(self):
+        pass
