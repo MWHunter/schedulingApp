79d85b674ad084613317eeefd7593db5dd42c171
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-12-03 19:28:46 -0600

    fix compilation from a missing import

--- schedulingApp/views.py
+++ schedulingApp/views.py
@@ -1,8 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
 
 from schedulingApp.models import Profile
