433c4ee972497b8ea9b5b32ea235dad1514af4ff
MWHunter <s0521458@student.rockvalleycollege.edu>
2022-12-19 12:10:01 -0600

    skills methods

--- schedulingApp/models.py
+++ schedulingApp/models.py
@@ -33,6 +33,12 @@ class Profile(models.Model):
                                   choices=PermissionLevel,
                                   default=TA)
 
+    def setSkills(self, skills):
+        self.skills = skills
+
+    def getSkills(self):
+        return self.skills
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
