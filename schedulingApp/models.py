62a4e5dd9c69a53de69f8066c631766ccd4a1289
tmtocco <98406643+tmtocco@users.noreply.github.com>
2022-12-20 04:08:49 -0600

    Update models.py

--- schedulingApp/models.py
+++ schedulingApp/models.py
@@ -33,6 +33,62 @@ class Profile(models.Model):
                                   choices=PermissionLevel,
                                   default=TA)
 
+   def getUsername(self):
+        return self.user.username
+
+    def getUserName(self):
+        return self.user.first_name
+
+    def getFirstName(self):
+        return self.user.first_name
+
+    def getLastName(self):
+        return self.user.last_name
+
+    def getEmailAddress(self):
+        return self.user.email
+
+    def getPassword(self):
+        return self.user.password
+
+    def getHomeAddress(self):
+        return self.homeAddress
+
+    def getPhoneNumber(self):
+        return self.phoneNumber
+
+    def getPermission(self):
+        return self.permission
+
+    def getSkills(self):
+        return self.skills
+    
+       def setUsername(self, newUsername):
+        self.user.username = newUsername
+
+    def setFirstName(self, newFirstName):
+        self.user.first_name = newFirstName
+
+    def setLastName(self, newLastName):
+        self.user.last_name = newLastName
+
+    def setEmailAddress(self, newEmailAddress):
+        self.user.email = newEmailAddress
+
+    def setPassword(self, newPassword):
+        self.user.password = newPassword
+
+    def setHomeAddress(self, newHomeAddress):
+        self.homeAddress = newHomeAddress
+
+    def setPhoneNumber(self, newPhoneNumber):
+        self.phoneNumber = newPhoneNumber
+
+    def setPermission(self, newPermission):
+        self.permission = newPermission
+
+    def setSkills(self, newSkills):
+        self.skills = newSkills
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
